bebb70b9f86b0465e563a772e05d268f6a11ff11
v_wenjinguo <13750712917@163.com>
2020-12-30 15:31:05 +0800

    爬虫2

--- src/浏览器操作.py
+++ src/浏览器操作.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from time import sleep
-import requests
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(3) # 隐式等待，超出时间还没有找到元素，将抛出异常
--- src/爬虫.py
+++ src/爬虫.py
@@ -5,9 +5,9 @@ from time import sleep
 
 x ={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 # 发送请求
-a = requests.get(url = 'https://www.zhipin.com/c101280600-p100309/?ka=search_100309')
+a = requests.get(url = 'https://www.f96.net/48/48193/')
 # 将获取的响应数据赋给变量b
-b = a.content.decode(encoding = 'utf-8')# encoding = 'gbk' 是该网页的编码方式
+b = a.content.decode(encoding = 'gbk')# encoding = 'gbk' 是该网页的编码方式
 print(b)# 打印获取到的数据
 # a1 = re.compile(r'<a href="(.*?)" rel="nofollow">(.*?)</a>')# 编写正则规则
 # b1 = re.findall(a1,b)# 使用正则规则a1去匹配字符串b中的内容
