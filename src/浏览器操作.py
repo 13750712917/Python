507f29a06a03c38e2137fdb45b03084ef9316070
v_wenjinguo <13750712917@163.com>
2020-12-30 10:43:38 +0800

    web自动化测试学习

--- src/浏览器操作.py
+++ src/浏览器操作.py
@@ -6,11 +6,6 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(3) # 隐式等待，超出时间还没有找到元素，将抛出异常
 driver.get("http://www.baidu.com")
 
-
-# 对网页进行操作，定位到元素，再跟上相应的动作
-driver.find_element_by_xpath('//*[@id="kw"]').click()
-sleep(2)
-
 driver.refresh() #刷新网页
 
 
