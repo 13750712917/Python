4a6b115f05bf9b4bee3f6c2a358cf2800203a5ba
WenJinGuo <13750712917@163.com>
2021-01-05 16:27:38 +0800

    小更改

---
+++ src/累加.py
@@ -0,0 +1,16 @@
+from time import sleep
+import os
+
+# a = os.system("adb shell pm list packages -3")
+# print(type(a))
+# print(a)
+
+a = 1000 # 全局变量
+a1 = 10
+a2 = 20
+
+def cha(x1 = 100,x2 = 99):
+    b=10000 # 局部变量
+    chazhi=a-x1-x2
+    return chazhi
+print(cha())
